7e622080bdcf8217423eed90cfe351caaa03e265
Мещеряков Степан <kosmo.zlo@yandex.ru>
2022-04-13 14:11:46 +0400

    lesson 9

--- src/lesson9/task1/Main.java
+++ src/lesson9/task1/Main.java
@@ -1,5 +1,7 @@
 package lesson9.task1;
 
+import sun.security.util.Password;
+
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,13 +16,13 @@ public class Main {
         Scanner loginIn = new Scanner(System.in);
         login = loginIn.nextLine();
 
-//        System.out.println("Введите пароль");
-//        Scanner passwordIn = new Scanner(System.in);
-//        password = passwordIn.nextLine();
-//
-//        System.out.println("Введите пароль ещё раз");
-//        Scanner confirmPasswordIn = new Scanner(System.in);
-//        confirmPassword = confirmPasswordIn.nextLine();
+        System.out.println("Введите пароль");
+        Scanner passwordIn = new Scanner(System.in);
+        password = passwordIn.nextLine();
+
+        System.out.println("Введите пароль ещё раз");
+        Scanner confirmPasswordIn = new Scanner(System.in);
+        confirmPassword = confirmPasswordIn.nextLine();
 
         logIn(login,password,confirmPassword);
     }
@@ -28,16 +30,26 @@ public class Main {
     public static boolean logIn(String login, String password, String confirmPassword) {
         String regLogin= "[a-zA-Z]+([_]+[0-9]+|[0-9]+[_]+)";
         Pattern pattern = Pattern.compile(regLogin);
-        Matcher matcher = pattern.matcher(login);
+        Matcher matcherLogin = pattern.matcher(login);
+        Matcher matcherPassword = pattern.matcher(password);
         try {
-            if (matcher.matches() && login.length() < 20) {
+            if (matcherLogin.matches() && login.length() < 20) {
                 System.out.println("Логин пдтверждён");
-                throw new WrongPasswordException(100,"не верный формат логина");
-                return true;
             } else {
-                throw new WrongLoginException(100, "не верный формат логина");
+                throw new WrongLoginException(100, "login incorrect");
             }
 
+            if (!matcherPassword.matches() && !(password.length() < 20)){
+                throw new WrongPasswordException(101, "password incorrect");
+            }
+
+            if (password.equals(confirmPassword)){
+                System.out.println("Пароль пдтверждён");
+            } else {
+                throw new WrongPasswordException(101, "passwords do not match");
+            }
+
+            return true;
         } catch (WrongLoginException | WrongPasswordException  e) {
             System.out.println("Exception: " + e.toString());
             return false;
